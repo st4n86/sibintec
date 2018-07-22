304f6f23aaf81fc841038cac5fbc29af82ad1f83
st4n86 <st4n86@gmail.com>
2018-07-22 23:12:40 +0700

    add jQuery

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -41,7 +41,7 @@
                 </div>
             </div>
         </div>
-        <script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+        <script src="{{ asset('js/jquery.js') }}"></script>
         <script src="{{ asset('js/script.js') }}"></script>
     </body>
 </html>
